02f7487d4d2616d6cd88b029092994910a431b0f
KisekiHirakawa <selkistak100@gmail.com>
2019-09-19 18:14:15 +0900

    added some possible API functions which we could use

---
+++ testAPI/test.py
@@ -0,0 +1,20 @@
+#Kindly add the functions that you think will be helpful. 
+
+#This Function gets the keywords that are needed for the routes
+def getKeywordsFromRoute(start_location, end_location, transport_medium):
+
+#This function gets the tweets from the keyword
+def getTweetsFromKeywords(keyword, no_of_tweets, location, range):
+
+#A function for relating the image to the keywords (from the exif data, etc)
+def getKeywordsFromImage():
+
+#This function obtains the location from the tweet
+def getLocationFromTweet():
+
+#This function compares the tweet location and cross-checks whether the tweet posted is geographically relevant to the keyword
+def verfiyTweetLocationWithKeyword():
+
+#This function categorises the tweet depending on its content
+def categoriseTweet(such as flood, traffic, roadblock):
+
