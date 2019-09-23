08a31ab60e5073a5be5955ac727fdca30a789f4a
KisekiHirakawa <selkistak100@gmail.com>
2019-09-23 21:17:27 +0900

    added getKeywordsFromRoute function

--- testAPI/test.py
+++ testAPI/test.py
@@ -1,7 +1,34 @@
 #Kindly add the functions that you think will be helpful. 
 
+#Importing necessary modules
+import urllib.request, json
+from bs4 import BeautifulSoup
+import re
+
+api_key ='Your API key'
+
 #This Function gets the keywords that are needed for the routes
-def getKeywordsFromRoute(start_location, end_location, transport_medium):
+def getKeywordsFromRoute(origin, destination):
+    #Google MapsDdirections API endpoint
+    endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+    
+    #Building the URL for the request
+    nav_request = 'origin={}&destination={}&key={}'.format(origin,destination,api_key)
+    request = endpoint + nav_request
+    
+    #Sends the request and reads the response.
+    response = urllib.request.urlopen(request).read()
+    
+    #Word list that stores all the keywords assosicated with a route
+    word_list = []
+    
+    #This step extracts the keywords which are within the <b></b> hashtag
+    for step in directions["routes"][0]["legs"][0]["steps"]:
+        soup = BeautifulSoup(step["html_instructions"], "lxml")
+        for node in soup.findAll('b'):
+            word_list.append((''.join(node.findAll(text=True))))
+            
+    return word_list 
 
 #This function gets the tweets from the keyword
 def getTweetsFromKeywords(keyword, no_of_tweets, location, range):
@@ -18,3 +45,4 @@ def verfiyTweetLocationWithKeyword():
 #This function categorises the tweet depending on its content
 def categoriseTweet(such as flood, traffic, roadblock):
 
+#
