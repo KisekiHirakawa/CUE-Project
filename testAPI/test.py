1a93c12d54b0e623e43c5662cc66d92896d37844
Sanjan Das <sanjandazz@gmail.com>
2019-09-24 11:05:29 +0530

    added a bit of documentation. Introduced a second parser option for bs4 since lxml wasn't working for me. Also, spaces in the origin and destination don't work well with the API so I replaced the spaces with +

--- testAPI/test.py
+++ testAPI/test.py
@@ -6,10 +6,25 @@ from bs4 import BeautifulSoup
 import re
 
 api_key ='Your API key'
+api_key = 'AIzaSyDdKHEi9ol4yd03Wly5ddGgNwQgDtctbr4'
 
 #This Function gets the keywords that are needed for the routes
 def getKeywordsFromRoute(origin, destination):
-    #Google MapsDdirections API endpoint
+    """
+    Parameters:
+        origin (str): contains the origin location
+        destination (str): contains the destination location
+        
+    Output:
+        word_list (list): Contains the directions, road names, etc sequentially
+    """
+    
+    # NOTE: Found that this API doesn't work well with origin/destination strings that have spaces in them
+    # and so need to replace the spaces by a '+'
+    origin.replace(' ', '+')
+    destination.replace(' ', '+')
+    
+    #Google Maps Ddirections API endpoint
     endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
     
     #Building the URL for the request
@@ -25,27 +40,40 @@ def getKeywordsFromRoute(origin, destination):
     #Word list that stores all the keywords assosicated with a route
     word_list = []
     
+    parsers = ['lxml', 'html.parser'] 
+    # @Kiseki, I think lxml worked for you, but wasn't for me so I added html.parser
+    
     #This step extracts the keywords which are within the <b></b> hashtag
     for step in directions["routes"][0]["legs"][0]["steps"]:
-        soup = BeautifulSoup(step["html_instructions"], "lxml")
+        soup = BeautifulSoup(step["html_instructions"], parsers[1])
         for node in soup.findAll('b'):
             word_list.append((''.join(node.findAll(text=True))))
             
     return word_list 
 
+
+
+
+
+
 #This function gets the tweets from the keyword
 def getTweetsFromKeywords(keyword, no_of_tweets, location, range):
+    return 0
 
 #A function for relating the image to the keywords (from the exif data, etc)
 def getKeywordsFromImage():
+    return 0
 
 #This function obtains the location from the tweet
 def getLocationFromTweet():
+    return 0
 
 #This function compares the tweet location and cross-checks whether the tweet posted is geographically relevant to the keyword
 def verfiyTweetLocationWithKeyword():
+    return 0
 
 #This function categorises the tweet depending on its content
-def categoriseTweet(such as flood, traffic, roadblock):
+def categoriseTweet(flood, traffic, roadblock):
+    return 0
 
 #
